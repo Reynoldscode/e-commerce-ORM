1da603584083acd62db2c5cc78fa7d079886020a
Reynolds Addy <reynoldsfrimpong@gmail.com>
2024-05-27 22:49:46 -0500

    initialize Product setup fields and rules for product model

--- models/index.js
+++ models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
-// Categories have many Products
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag, 
+  foreignKey: 'product_id'
+});
 
-// Tags belongToMany Products (through ProductTag)
+// A Category has many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // The foreign key in the Product model
+});
 
 module.exports = {
   Product,
